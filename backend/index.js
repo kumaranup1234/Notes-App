844fab4ba63748f2e88b37982582fc8ae54e7886
kumaranup1234 <anupg2551@gmail.com>
2024-05-31 15:01:52 +0530

    Modified the index.js

--- backend/index.js
+++ backend/index.js
@@ -2,25 +2,26 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-app.use(express.json());
 const { connectToMongoDB } = require("./database");
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+app.use(express.json());
 
-
+// Configure CORS
 app.use(cors({
     origin: "https://notes-app-frontend-navy.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-}))
+}));
 
-
+// Set JWT secret
 app.set("jwtSecret", process.env.ACCESS_TOKEN_SECRET);
 
+// Import and use routes
 const router = require("./routes");
 app.use("/", router);
 
-
+// Proxy middleware
 app.use(
     '/proxy',
     createProxyMiddleware({
@@ -29,13 +30,19 @@ app.use(
     })
 );
 
-
+// Define port
 const port = process.env.PORT || 8000;
 
+// Start the server
 async function startServer() {
-    await connectToMongoDB();
-    app.listen(port, () => {
-        console.log(`Server is listening on http://localhost:${port}`);
-    });
+    try {
+        await connectToMongoDB();
+        app.listen(port, () => {
+            console.log(`Server is listening on http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.error("Failed to start the server:", error);
+    }
 }
+
 startServer();
